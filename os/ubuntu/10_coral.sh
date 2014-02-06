cefea8278548ac815e61fff681836abbf804c051
Adrian Webb <adrian.webb@coraltech.net>
2014-02-06 17:37:58 -0500

    Adding the current active branch in the clone of the coral bootstrap process.

--- os/ubuntu/10_coral.sh
+++ os/ubuntu/10_coral.sh
@@ -4,7 +4,7 @@
 # Install the Coral core gem
 mkdir /tmp/coral || exit 100
 
-git clone git://github.com/coralnexus/coral_core.git /tmp/coral || exit 101
+git clone --branch 0.2 git://github.com/coralnexus/coral_core.git /tmp/coral || exit 101
 gem install /tmp/coral/coral_core-*.gem || exit 102
 
 rm -Rf /tmp/coral || exit 103
