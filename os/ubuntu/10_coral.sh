fccc37dcbf62dad5080478c47703136ba4933733
Adrian Webb <adrian.webb@coraltech.net>
2014-02-11 05:07:32 -0500

    Updating the coral installer to remove the tmp directory before creating to avoid errors.

--- os/ubuntu/10_coral.sh
+++ os/ubuntu/10_coral.sh
@@ -2,9 +2,8 @@
 #-------------------------------------------------------------------------------
 
 # Install the Coral core gem
+rm -Rf /tmp/coral
 mkdir /tmp/coral || exit 100
 
 git clone --branch 0.2 git://github.com/coralnexus/ruby-coral_core.git /tmp/coral || exit 101
 gem install /tmp/coral/coral_core-*.gem || exit 102
-
-rm -Rf /tmp/coral || exit 103
