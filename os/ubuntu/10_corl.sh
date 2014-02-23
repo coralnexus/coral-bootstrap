6b790cae27333fb56bdd2faa00030519cd306edb
Adrian Webb <adrian.webb@coraltech.net>
2014-02-23 03:42:24 -0500

    Adding manual installation of the nucleon gem to the corl script for testing purposes.

--- os/ubuntu/10_corl.sh
+++ os/ubuntu/10_corl.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
 
+# For now we install these through the repos for testing purposes
+
+# Install the nucleon gem
+rm -Rf /tmp/nucleon
+mkdir /tmp/nucleon || exit 100
+
+git clone --branch 0.1 git://github.com/coralnexus/nucleon.git /tmp/nucleon || exit 101
+gem install /tmp/nucleon/nucleon-*.gem || exit 102
+
 # Install the corl gem
 rm -Rf /tmp/corl
-mkdir /tmp/corl || exit 100
+mkdir /tmp/corl || exit 103
 
-git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 101
-gem install /tmp/corl/corl-*.gem || exit 102
+git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 104
+gem install /tmp/corl/corl-*.gem || exit 105
