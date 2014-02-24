380bcffaed1e14259eac21984b9a476bc863fcdf
Adrian Webb <adrian.webb@coraltech.net>
2014-02-23 23:20:38 -0500

    Splitting up nucleon and CORL installation and adding submodule initialization.

---
+++ os/ubuntu/09_nucleon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+
+# For now we install Nucleon through the repo for testing purposes
+
+# Install the nucleon gem
+rm -Rf /tmp/nucleon
+mkdir /tmp/nucleon || exit 90
+
+cd /tmp/nucleon || exit 101
+git clone --branch 0.1 git://github.com/coralnexus/nucleon.git /tmp/nucleon || exit 91
+git submodule update --init --recursive || exit 92
+gem build /tmp/nucleon/nucleon.gemspec || exit 93
+gem install /tmp/nucleon/nucleon-*.gem || exit 94
--- os/ubuntu/10_corl.sh
+++ os/ubuntu/10_corl.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
 
-# For now we install these through the repos for testing purposes
-
-# Install the nucleon gem
-rm -Rf /tmp/nucleon
-mkdir /tmp/nucleon || exit 100
-
-cd /tmp/nucleon || exit 101
-git clone --branch 0.1 git://github.com/coralnexus/nucleon.git /tmp/nucleon || exit 102
-gem build /tmp/nucleon/nucleon.gemspec || exit 103
-gem install /tmp/nucleon/nucleon-*.gem || exit 104
+# For now we install CORL through the repo for testing purposes
 
 # Install the corl gem
 rm -Rf /tmp/corl
-mkdir /tmp/corl || exit 105
+mkdir /tmp/corl || exit 100
 
-cd /tmp/corl || exit 106
-git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 107
-gem build /tmp/corl/corl.gemspec || exit 108
-gem install /tmp/corl/corl-*.gem || exit 109
+cd /tmp/corl || exit 101
+git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 102
+git submodule update --init --recursive || exit 103
+gem build /tmp/corl/corl.gemspec || exit 104
+gem install /tmp/corl/corl-*.gem || exit 105
