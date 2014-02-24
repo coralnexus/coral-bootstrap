3b528c6f41d4e6badcbbb1e5ac3b1c871fb71a02
Adrian Webb <adrian.webb@coraltech.net>
2014-02-23 23:11:49 -0500

    Attempting to fix gem build issues in the corl script.

--- os/ubuntu/10_corl.sh
+++ os/ubuntu/10_corl.sh
@@ -7,14 +7,16 @@
 rm -Rf /tmp/nucleon
 mkdir /tmp/nucleon || exit 100
 
-git clone --branch 0.1 git://github.com/coralnexus/nucleon.git /tmp/nucleon || exit 101
-gem build /tmp/nucleon/nucleon.gemspec || exit 102
-gem install /tmp/nucleon/nucleon-*.gem || exit 103
+cd /tmp/nucleon || exit 101
+git clone --branch 0.1 git://github.com/coralnexus/nucleon.git /tmp/nucleon || exit 102
+gem build /tmp/nucleon/nucleon.gemspec || exit 103
+gem install /tmp/nucleon/nucleon-*.gem || exit 104
 
 # Install the corl gem
 rm -Rf /tmp/corl
-mkdir /tmp/corl || exit 104
+mkdir /tmp/corl || exit 105
 
-git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 105
-gem build /tmp/corl/corl.gemspec || exit 106
-gem install /tmp/corl/corl-*.gem || exit 107
+cd /tmp/corl || exit 106
+git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 107
+gem build /tmp/corl/corl.gemspec || exit 108
+gem install /tmp/corl/corl-*.gem || exit 109
