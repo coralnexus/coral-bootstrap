d097a8f0cf3ff9457a3fe02fcfaf20043aac87b1
Adrian Webb <adrian.webb@coraltech.net>
2014-02-23 22:53:08 -0500

    Adding building of gems to the corl bootstrap script so we can more easily test changes.

--- os/ubuntu/10_corl.sh
+++ os/ubuntu/10_corl.sh
@@ -8,11 +8,13 @@ rm -Rf /tmp/nucleon
 mkdir /tmp/nucleon || exit 100
 
 git clone --branch 0.1 git://github.com/coralnexus/nucleon.git /tmp/nucleon || exit 101
-gem install /tmp/nucleon/nucleon-*.gem || exit 102
+gem build /tmp/nucleon/nucleon.gemspec || exit 102
+gem install /tmp/nucleon/nucleon-*.gem || exit 103
 
 # Install the corl gem
 rm -Rf /tmp/corl
-mkdir /tmp/corl || exit 103
+mkdir /tmp/corl || exit 104
 
-git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 104
-gem install /tmp/corl/corl-*.gem || exit 105
+git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 105
+gem build /tmp/nucleon/corl.gemspec || exit 106
+gem install /tmp/corl/corl-*.gem || exit 107
