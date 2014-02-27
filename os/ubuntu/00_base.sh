e9b23380cc060d74243f66d807e20ce04fd93113
Adrian Webb <adrian.webb@coraltech.net>
2014-02-26 19:54:31 -0500

    Adding setting of hostname and updating error codes in the ubuntu base bootstrap script.

--- os/ubuntu/00_base.sh
+++ os/ubuntu/00_base.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
 
-echo "Host name!!!"
-echo $HOSTNAME
+# Set hostname
+echo "$HOSTNAME" > "/etc/hostname" || exit 1
 
 # Update system packages
-apt-get update
+apt-get update || exit 2
 
 # Install basic build packages.
-apt-get -y install build-essential || exit 1
+apt-get -y install build-essential || exit 3
