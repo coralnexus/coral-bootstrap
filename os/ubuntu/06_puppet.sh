6e4e1a78eec89fde4c29b83432d6d40bc25bf78f
Adrian Webb <adrian.webb@coraltech.net>
2014-02-26 19:55:33 -0500

    Fixing some names in the ubuntu puppet bootstrap script.

--- os/ubuntu/06_puppet.sh
+++ os/ubuntu/06_puppet.sh
@@ -8,8 +8,8 @@ apt-key adv --recv-key --keyserver pgp.mit.edu 4BD6EC30 || exit 60
 deb http://apt.puppetlabs.com precise main dependencies
 deb-src http://apt.puppetlabs.com precise main dependencies
 EOP
-) > /etc/apt/sources.list.d/coral_puppet.list || exit 61
-chmod 0644 /etc/apt/sources.list.d/coral_puppet.list || exit 62
+) > /etc/apt/sources.list.d/puppet.list || exit 61
+chmod 0644 /etc/apt/sources.list.d/puppet.list || exit 62
 
 # Install Puppet
 apt-get -y update || exit 63
@@ -17,7 +17,7 @@ apt-get -y install puppet || exit 64
 gem install libshadow || exit 65
 
 # Set up Hiera configuration
-mkdir -p /var/coral/config || exit 66
+mkdir -p /var/corl/config || exit 66
 
 ( cat <<'EOP'
 ---
@@ -26,13 +26,13 @@ mkdir -p /var/coral/config || exit 66
   - yaml
   - json
 :yaml:
-  :datadir: /var/coral/config
+  :datadir: /var/corl/config
 :json:
-  :datadir: /var/coral/config
+  :datadir: /var/corl/config
 :hierarchy:
   - common
 EOP
 ) > /etc/hiera.yaml || exit 67
 chmod 0440 /etc/hiera.yaml || exit 68
 
-cp /etc/hiera.yaml /etc/puppet/hiera.yaml || exit 69
+ln -s /etc/hiera.yaml /etc/puppet/hiera.yaml || exit 69
