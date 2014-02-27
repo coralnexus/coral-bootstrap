dbf491669fbff39fbb95a051965e864e3fe2d8f9
Adrian Webb <adrian.webb@coraltech.net>
2014-02-26 19:55:10 -0500

    Minor comment changes to the ubuntu ruby bootstrap script.

--- os/ubuntu/05_ruby.sh
+++ os/ubuntu/05_ruby.sh
@@ -6,7 +6,7 @@ apt-get -y install ruby1.9.1 ruby1.9.1-dev || exit 50
 update-alternatives --set ruby /usr/bin/ruby1.9.1 || exit 51
 update-alternatives --set gem /usr/bin/gem1.9.1 || exit 52
 
-# Set gem options
+# Set Gem options
 ( cat <<'EOP'
 gem: --no-document
 EOP
