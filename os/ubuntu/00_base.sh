8edb02dd0a5199af697e40bd6ce93aba52546ad8
Adrian Webb <adrian.webb@coraltech.net>
2014-02-26 19:46:03 -0500

    Adding updating of packages to the ubuntu base bootstrap script.

--- os/ubuntu/00_base.sh
+++ os/ubuntu/00_base.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
 
+echo "Host name!!!"
+echo $HOSTNAME
+
+# Update system packages
+apt-get update
+
 # Install basic build packages.
 apt-get -y install build-essential || exit 1
