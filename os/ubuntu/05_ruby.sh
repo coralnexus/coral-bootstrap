a3eed141fb4b9a382b114e03786337991751e9cf
Adrian Webb <adrian.webb@coraltech.net>
2014-02-26 19:47:02 -0500

    Adding setting of gem options in the ubuntu ruby bootstrap script.

--- os/ubuntu/05_ruby.sh
+++ os/ubuntu/05_ruby.sh
@@ -5,3 +5,9 @@
 apt-get -y install ruby1.9.1 ruby1.9.1-dev || exit 50
 update-alternatives --set ruby /usr/bin/ruby1.9.1 || exit 51
 update-alternatives --set gem /usr/bin/gem1.9.1 || exit 52
+
+# Set gem options
+( cat <<'EOP'
+gem: --no-document
+EOP
+) > "$HOME/.gemrc" || exit 53
