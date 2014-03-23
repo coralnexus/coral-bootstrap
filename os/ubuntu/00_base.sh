d9de282eb13f81b3269842f0781968b69e5e9411
Adrian Webb <adrian.webb@coraltech.net>
2014-03-22 23:09:53 -0400

    Adding a default OpenDNS resolve entry in the base bootstrap script.

--- os/ubuntu/00_base.sh
+++ os/ubuntu/00_base.sh
@@ -7,8 +7,11 @@ echo "$HOSTNAME" > "/etc/hostname" || exit 1
 sed -ri 's/127\.0\.1\.1.*//' /etc/hosts
 echo "127.0.1.1 $HOSTNAME" >> /etc/hosts || exit 2
 
+# Set OpenDNS as our DNS lookup source
+echo "nameserver 208.67.222.222" | tee /etc/resolvconf/resolv.conf.d/base > /dev/null || exit 3
+
 # Update system packages
-apt-get update || exit 3
+apt-get update || exit 4
 
 # Install basic build packages.
-apt-get -y install build-essential || exit 4
+apt-get -y install build-essential || exit 5
