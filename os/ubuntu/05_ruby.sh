47f0f09995c24a43d3b42e68c90f1288d1e6c913
Adrian Webb <adrian.webb@coraltech.net>
2014-04-03 02:22:44 -0400

    Adding an apt update in the ruby bootstrap script.

--- os/ubuntu/05_ruby.sh
+++ os/ubuntu/05_ruby.sh
@@ -2,14 +2,15 @@
 #-------------------------------------------------------------------------------
 
 # Install Ruby.
-add-apt-repository ppa:brightbox/ruby-ng-experimental
+add-apt-repository ppa:brightbox/ruby-ng-experimental 2>&1 || exit 51
+apt-get update || exit 52
 
-apt-get -y install ruby2.1 ruby2.1-dev || exit 51
-update-alternatives --set ruby /usr/bin/ruby2.1 || exit 52
-update-alternatives --set gem /usr/bin/gem2.1 || exit 53
+apt-get -y install ruby2.1 ruby2.1-dev || exit 53
+update-alternatives --set ruby /usr/bin/ruby2.1 || exit 54
+update-alternatives --set gem /usr/bin/gem2.1 || exit 55
 
 # Set Gem options
 ( cat <<'EOP'
 gem: --no-rdoc --no-ri 
 EOP
-) > "$HOME/.gemrc" || exit 54
+) > "$HOME/.gemrc" || exit 56
