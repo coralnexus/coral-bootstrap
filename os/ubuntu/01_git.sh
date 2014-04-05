23427c1094edd9c7a6d7688c95361b895cd63929
Adrian Webb <adrian.webb@coraltech.net>
2014-04-05 19:25:11 -0400

    Initializing or resetting github.com hnown_hosts entry in the git bootstrap script.

--- os/ubuntu/01_git.sh
+++ os/ubuntu/01_git.sh
@@ -3,3 +3,7 @@
 
 # Install Git.
 apt-get -y install git || exit 10
+
+# Make sure it is easy to communicate with repo hosts
+ssh-keygen -R github.com 2>&1 || exit 11 # No duplicates
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts || exit 12
