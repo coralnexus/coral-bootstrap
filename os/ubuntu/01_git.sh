bbdbaf6b2f4203a335ea1e47d6a2008bae3c98cc
Adrian Webb <adrian.webb@coraltech.net>
2014-04-05 22:47:49 -0400

    Fixing non existent file issues in the git bootstrap script.

--- os/ubuntu/01_git.sh
+++ os/ubuntu/01_git.sh
@@ -4,6 +4,11 @@
 # Install Git.
 apt-get -y install git || exit 10
 
+echo "Hello $USER"
+
 # Make sure it is easy to communicate with repo hosts
-ssh-keygen -R github.com 2>&1 || exit 11 # No duplicates
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts || exit 12
+mkdir -p ~/.ssh || exit 11
+touch ~/.ssh/known_hosts || exit 12
+
+ssh-keygen -R github.com 2>&1 || exit 13 # No duplicates
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts || exit 14
