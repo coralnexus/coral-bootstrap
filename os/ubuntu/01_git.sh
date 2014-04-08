14099f5119fc5c033310ff069b994909773ccd59
Adrian Webb <adrian.webb@coraltech.net>
2014-04-08 18:33:41 -0400

    Fixing root level known_hosts issues in the git bootstrap script.

--- os/ubuntu/01_git.sh
+++ os/ubuntu/01_git.sh
@@ -4,11 +4,9 @@
 # Install Git.
 apt-get -y install git || exit 10
 
-echo "Hello $USER"
-
 # Make sure it is easy to communicate with repo hosts
-mkdir -p ~/.ssh || exit 11
-touch ~/.ssh/known_hosts || exit 12
+mkdir -p "/root/.ssh" || exit 11
+touch "/root/.ssh/known_hosts" || exit 12
 
 ssh-keygen -R github.com 2>&1 || exit 13 # No duplicates
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts || exit 14
+ssh-keyscan -H github.com >> "/root/.ssh/known_hosts" || exit 14
