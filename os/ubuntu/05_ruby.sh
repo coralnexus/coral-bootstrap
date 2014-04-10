77dab33819a4fe1d55a9253c92506de175afbcfd
Adrian Webb <adrian.webb@coraltech.net>
2014-04-10 04:33:09 -0400

    Reverting the Ruby version to 2.0 until GC can be better handled in Puppet.

--- os/ubuntu/05_ruby.sh
+++ os/ubuntu/05_ruby.sh
@@ -5,9 +5,9 @@
 add-apt-repository -y ppa:brightbox/ruby-ng-experimental 2>&1 || exit 51
 apt-get update || exit 52
 
-apt-get -y install ruby2.1 ruby2.1-dev || exit 53
-update-alternatives --set ruby /usr/bin/ruby2.1 || exit 54
-update-alternatives --set gem /usr/bin/gem2.1 || exit 55
+apt-get -y install ruby2.0 ruby2.0-dev || exit 53
+update-alternatives --set ruby /usr/bin/ruby2.0 || exit 54
+update-alternatives --set gem /usr/bin/gem2.0 || exit 55
 
 # Set Gem options
 ( cat <<'EOP'
