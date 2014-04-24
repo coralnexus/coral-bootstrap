2aba83832721d8201252e70b59f4e0651b08f017
Adrian Webb <adrian.webb@coraltech.net>
2014-04-23 22:43:33 -0400

    Eliminating weird issue with Nokogiri not building through corl gemspec.

--- os/ubuntu/10_corl.sh
+++ os/ubuntu/10_corl.sh
@@ -3,12 +3,17 @@
 
 # For now we install CORL through the repo for testing purposes
 
+# Install this dependency separately so the damn thing will build.
+# I REALLY dislike Nokogiri.
+# TODO: Figure out why this is happening?
+gem install nokogiri || exit 100
+
 # Install the corl gem
 rm -Rf /tmp/corl
-mkdir /tmp/corl || exit 100
+mkdir /tmp/corl || exit 101
 
-cd /tmp/corl || exit 101
-git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 102
-git submodule update --init --recursive || exit 103
-gem build /tmp/corl/corl.gemspec || exit 104
-gem install /tmp/corl/corl-*.gem || exit 105
+cd /tmp/corl || exit 102
+git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 103
+git submodule update --init --recursive || exit 104
+gem build /tmp/corl/corl.gemspec || exit 105
+gem install /tmp/corl/corl-*.gem || exit 106
