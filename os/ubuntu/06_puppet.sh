912a1155ff43c698feaf8eb19c5589b3ebccdd74
Adrian Webb <adrian.webb@coraltech.net>
2014-04-24 21:12:19 -0400

    Splitting Puppet settings for different Ubuntu LTS versions.

--- os/ubuntu/06_puppet.sh
+++ os/ubuntu/06_puppet.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
 
+case "$OS_VERSION" in
+'14.04')
+    PUPPET_PACKAGE="3.4.3-1"
+;;
+'12.04')
+    PUPPET_PACKAGE="3.4.3-1puppetlabs1"
+;;
+esac
+
+#---
+
 # Set up Puppet Apt repositories
 apt-key adv --recv-key --keyserver pgp.mit.edu 4BD6EC30 2>&1 || exit 60
 
-( cat <<'EOP'
-deb http://apt.puppetlabs.com trusty main dependencies
-deb-src http://apt.puppetlabs.com trusty main dependencies
-EOP
-) > /etc/apt/sources.list.d/puppet.list || exit 61
+echo -e "deb http://apt.puppetlabs.com $OS_NAME main dependencies\ndeb-src http://apt.puppetlabs.com $OS_NAME main dependencies" | cat > /etc/apt/sources.list.d/puppet.list || exit 61
 chmod 0644 /etc/apt/sources.list.d/puppet.list || exit 62
 
 # Install Puppet
 apt-get -y update || exit 63
-apt-get -y install puppet-common=3.4.3-1 puppet=3.4.3-1 || exit 64
+
+apt-get -y install puppet-common="$PUPPET_PACKAGE" puppet="$PUPPET_PACKAGE" || exit 64
 
 # Set up Hiera configuration
 mkdir -p /var/corl/config || exit 65
