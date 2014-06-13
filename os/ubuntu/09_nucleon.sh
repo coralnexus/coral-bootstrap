b7568ac19eb2ef75f7977f6753a14d1f5ce8f570
Adrian Webb <adrian.webb@coralnexus.com>
2014-06-13 18:13:49 -0400

    Updating the nucleon branch in the ubuntu nucleon bootstrap script.

--- os/ubuntu/09_nucleon.sh
+++ os/ubuntu/09_nucleon.sh
@@ -8,7 +8,7 @@ rm -Rf /tmp/nucleon
 mkdir /tmp/nucleon || exit 90
 
 cd /tmp/nucleon || exit 101
-git clone --branch 0.1 git://github.com/coralnexus/nucleon.git /tmp/nucleon || exit 91
+git clone --branch 0.2 git://github.com/coralnexus/nucleon.git /tmp/nucleon || exit 91
 git submodule update --init --recursive || exit 92
 gem build /tmp/nucleon/nucleon.gemspec || exit 93
 gem install /tmp/nucleon/nucleon-*.gem || exit 94
