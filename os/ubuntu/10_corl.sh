ed56a0959f7b955cfe6a59a88acc3036e35d7e4f
Adrian Webb <adrian.webb@coralnexus.com>
2014-06-13 18:15:41 -0400

    Updating the CORL branch in the Ubuntu CORL bootstrap script.

--- os/ubuntu/10_corl.sh
+++ os/ubuntu/10_corl.sh
@@ -3,9 +3,6 @@
 
 # For now we install CORL through the repo for testing purposes
 
-# Install this dependency separately so the damn thing will build.
-# I REALLY dislike Nokogiri.
-# TODO: Figure out why this is happening?
 gem install nokogiri || exit 100
 
 # Install the corl gem
@@ -13,7 +10,7 @@ rm -Rf /tmp/corl
 mkdir /tmp/corl || exit 101
 
 cd /tmp/corl || exit 102
-git clone --branch 0.4 git://github.com/coralnexus/corl.git /tmp/corl || exit 103
+git clone --branch 0.5 git://github.com/coralnexus/corl.git /tmp/corl || exit 103
 git submodule update --init --recursive || exit 104
 gem build /tmp/corl/corl.gemspec || exit 105
 gem install /tmp/corl/corl-*.gem || exit 106
