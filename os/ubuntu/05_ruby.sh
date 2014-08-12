c8358c04d023156d9a12bb3a03c744ccaaef8fbd
Adrian Webb <adrian.webb@coraltech.net>
2014-08-11 23:47:30 -0400

    Now installing Ruby 2.1.2 from source in the ubuntu ruby bootstrap script.

--- os/ubuntu/05_ruby.sh
+++ os/ubuntu/05_ruby.sh
@@ -2,19 +2,13 @@
 #-------------------------------------------------------------------------------
 
 # Install Ruby.
-case "$OS_VERSION" in
-'14.04')
-    apt-get -y install ruby1.9.1 ruby1.9.1-dev || exit 50
-;;
-'12.04')
-    add-apt-repository -y ppa:brightbox/ruby-ng-experimental 2>&1 || exit 51
-    apt-get update || exit 52
+echo "Installing Ruby 2.1.2"
+wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz --output-document=/tmp/ruby2.1.2.tar.gz -q || exit 50
+tar -xzvf /tmp/ruby2.1.2.tar.gz --directory=/tmp 1>/dev/null || exit 51
 
-    apt-get -y install ruby2.0 ruby2.0-dev || exit 53
-    update-alternatives --set ruby /usr/bin/ruby2.0 || exit 54
-    update-alternatives --set gem /usr/bin/gem2.0 || exit 55
-;;
-esac
+/tmp/ruby-2.1.2/configure --quiet || exit 52
+make --directory=/tmp/ruby-2.1.2 --quiet || exit 53
+make install --directory=/tmp/ruby-2.1.2 --quiet || exit 54
 
 # Set Gem options
 ( cat <<'EOP'
