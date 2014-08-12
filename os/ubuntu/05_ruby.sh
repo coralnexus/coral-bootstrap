642c6d634604ec46bde5da1f5c4d46929535ee8a
Adrian Webb <adrian.webb@venturiscm.com>
2014-08-12 02:48:26 -0400

    Fixing critical issues with the ruby installation in the ubuntu ruby bootstrap script.

--- os/ubuntu/05_ruby.sh
+++ os/ubuntu/05_ruby.sh
@@ -3,15 +3,32 @@
 
 # Install Ruby.
 echo "Installing Ruby 2.1.2"
-wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz --output-document=/tmp/ruby2.1.2.tar.gz -q || exit 50
-tar -xzvf /tmp/ruby2.1.2.tar.gz --directory=/tmp 1>/dev/null || exit 51
+if [ ! -f /tmp/ruby2.1.2.tar.gz ]
+then
+    echo "Downloading Ruby tar archive"
+    wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz --output-document=/tmp/ruby2.1.2.tar.gz -q || exit 50
+    CORL_INSTALL_RUBY="yes"
+fi
+if [ ! -d /tmp/ruby-2.1.2 ]
+then
+    echo "Extracting Ruby tar archive"
+    tar -xzvf /tmp/ruby2.1.2.tar.gz --directory=/tmp 1>/dev/null || exit 51
+    CORL_INSTALL_RUBY="yes"
+fi
 
-/tmp/ruby-2.1.2/configure --quiet || exit 52
-make --directory=/tmp/ruby-2.1.2 --quiet || exit 53
-make install --directory=/tmp/ruby-2.1.2 --quiet || exit 54
+if [ "$CORL_INSTALL_RUBY" == "yes" ]
+then
+    cd /tmp/ruby-2.1.2 || exit 52
+    ./configure --quiet || exit 53
+    make --quiet || exit 54
+    make install --quiet || exit 55
+fi
 
+if [ ! -e "$HOME/.gemrc" ]
+then
 # Set Gem options
 ( cat <<'EOP'
 gem: --no-rdoc --no-ri 
 EOP
 ) > "$HOME/.gemrc" || exit 56
+fi
