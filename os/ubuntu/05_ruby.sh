91176aa33bf48714fcad348082f5857a2f4a0f31
Adrian Webb <adrian.webb@venturiscm.com>
2014-08-13 02:55:39 -0400

    Removing Rubinius from workflow until I can fix issues with rugged so symbols.

--- os/ubuntu/05_ruby.sh
+++ os/ubuntu/05_ruby.sh
@@ -25,7 +25,7 @@ then
     cd "/tmp/ruby-${CORL_RUBY_VERSION}" || exit 52
     
     echo "3. Configuring Ruby build"
-    ./configure --bindir=/usr/bin >/tmp/ruby.config.log 2>&1 || exit 53
+    ./configure --bindir=/usr/local/bin >/tmp/ruby.config.log 2>&1 || exit 53
     
     echo "4. Building Ruby"
     make >/tmp/ruby.make.log 2>&1  || exit 54
@@ -39,35 +39,35 @@ fi
 
 # Install Rubinius.
 
-CORL_RUBINIUS_BASE="2.2"
-CORL_RUBINIUS_PATCH="10"
-CORL_RUBINIUS_VERSION="${CORL_RUBINIUS_BASE}.${CORL_RUBINIUS_PATCH}"
+#CORL_RUBINIUS_BASE="2.2"
+#CORL_RUBINIUS_PATCH="10"
+#CORL_RUBINIUS_VERSION="${CORL_RUBINIUS_BASE}.${CORL_RUBINIUS_PATCH}"
 
-if [ ! -d "/tmp/rubinius-${CORL_RUBINIUS_VERSION}" ]
-then
-	echo "7. Fetching Rubinius repository"
-    git clone "git://github.com/rubinius/rubinius.git" --branch "v${CORL_RUBINIUS_VERSION}" "/tmp/rubinius-${CORL_RUBINIUS_VERSION}" >/tmp/rubinius.git.log 2>&1 || exit 57
-    CORL_INSTALL_RUBY="yes"
-fi
-
-if [ "$CORL_INSTALL_RUBY" == "yes" ]
-then
-    cd "/tmp/rubinius-${CORL_RUBINIUS_VERSION}" || exit 58
-    
-    echo "8. Ensuring Rubinius dependencies"
-    bundle install >/tmp/rubinius.bundle.log 2>&1 || exit 59
-    
-    echo "9. Configuring Rubinius build"
-    ./configure --prefix=/usr/local/rubinius --bindir=/usr/local/bin >/tmp/rubinius.config.log 2>&1 || exit 60
-    
-    echo "10. Installing Rubinius ${CORL_RUBINIUS_BASE}"
-    rake install >/tmp/rubinius.install.log 2>&1 || exit 61
-    
-    if [ ! -z "$SUDO_USER" ]
-    then
-    	chown -R $SUDO_USER:$SUDO_USER "$HOME/.rbx" || exit 62
-    fi
-fi
+#if [ ! -d "/tmp/rubinius-${CORL_RUBINIUS_VERSION}" ]
+#then
+#	echo "7. Fetching Rubinius repository"
+#    git clone "git://github.com/rubinius/rubinius.git" --branch "v${CORL_RUBINIUS_VERSION}" "/tmp/rubinius-${CORL_RUBINIUS_VERSION}" >/tmp/rubinius.git.log 2>&1 || exit 57
+#    CORL_INSTALL_RUBY="yes"
+#fi
+#
+#if [ "$CORL_INSTALL_RUBY" == "yes" ]
+#then
+#    cd "/tmp/rubinius-${CORL_RUBINIUS_VERSION}" || exit 58
+#    
+#    echo "8. Ensuring Rubinius dependencies"
+#    bundle install >/tmp/rubinius.bundle.log 2>&1 || exit 59
+#    
+#    echo "9. Configuring Rubinius build"
+#    bundle exec ./configure --prefix=/usr/local/rubinius --bindir=/usr/local/bin >/tmp/rubinius.config.log 2>&1 || exit 60
+#    
+#    echo "10. Installing Rubinius ${CORL_RUBINIUS_BASE}"
+#    bundle exec rake install >/tmp/rubinius.install.log 2>&1 || exit 61
+#    
+#    if [ ! -z "$SUDO_USER" ]
+#    then
+#    	chown -R $SUDO_USER:$SUDO_USER "$HOME/.rbx" || exit 62
+#    fi
+#fi
 
 if [ ! -e "$HOME/.gemrc" ]
 then
