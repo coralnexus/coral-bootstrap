a9dc4ea04bdb72cbe92182fde807f5fc14f831d0
Adrian Webb <adrian.webb@coraltech.net>
2014-11-29 18:11:42 -0500

    Adding a development build flag to the nucleon and corl bootstrap scripts.

--- os/ubuntu/09_nucleon.sh
+++ os/ubuntu/09_nucleon.sh
@@ -4,17 +4,27 @@
 # For now we install Nucleon through the repo for testing purposes
 
 # Install the nucleon gem
-rm -Rf /tmp/nucleon
-mkdir /tmp/nucleon || exit 90
 
-cd /tmp/nucleon || exit 101
+if [ "$DEVELOPMENT_BUILD" ]
+then
+  rm -Rf /tmp/nucleon
+  mkdir /tmp/nucleon || exit 90
 
-echo "1. Fetching Nucleon source repository"
-git clone --branch 0.2 git://github.com/coralnexus/nucleon.git /tmp/nucleon >/tmp/nucleon.git.log 2>&1 || exit 91
-git submodule update --init --recursive >>/tmp/nucleon.git.log 2>&1 || exit 92
+  cd /tmp/nucleon || exit 101
 
-echo "2. Building Nucleon gem from branch 0.2"
-gem build /tmp/nucleon/nucleon.gemspec >/tmp/nucleon.build.log 2>&1 || exit 93
+  echo "1. Fetching Nucleon source repository"
+  git clone --branch 0.2 git://github.com/coralnexus/nucleon.git /tmp/nucleon >/tmp/nucleon.git.log 2>&1 || exit 91
+  git submodule update --init --recursive >>/tmp/nucleon.git.log 2>&1 || exit 92
 
-echo "3. Installing latest Nucleon"
-gem install /tmp/nucleon/nucleon-*.gem >/tmp/nucleon.install.log 2>&1 || exit 94
+  echo "2. Building Nucleon gem from branch 0.2"
+  gem build /tmp/nucleon/nucleon.gemspec >/tmp/nucleon.build.log 2>&1 || exit 93
+
+  echo "3. Installing latest Nucleon"
+  gem install /tmp/nucleon/nucleon-*.gem >/tmp/nucleon.install.log 2>&1 || exit 94
+else
+  echo "1. Removing old versions of Nucleon gem"
+  gem uninstall nucleon --force
+  
+  echo "2. Installing latest release of Nucleon"
+  gem install nucleon  
+fi
--- os/ubuntu/10_corl.sh
+++ os/ubuntu/10_corl.sh
@@ -10,17 +10,27 @@ then
 fi
 
 # Install the corl gem
-rm -Rf /tmp/corl
-mkdir /tmp/corl || exit 101
 
-cd /tmp/corl || exit 102
+if [ "$DEVELOPMENT_BUILD" ]
+then
+  rm -Rf /tmp/corl
+  mkdir /tmp/corl || exit 101
 
-echo "2. Fetching CORL source repository"
-git clone --branch 0.5 git://github.com/coralnexus/corl.git /tmp/corl >/tmp/corl.git.log 2>&1 || exit 103
-git submodule update --init --recursive >>/tmp/corl.git.log 2>&1 || exit 104
+  cd /tmp/corl || exit 102
 
-echo "3. Building CORL gem from branch 0.5"
-gem build /tmp/corl/corl.gemspec >/tmp/corl.build.log 2>&1 || exit 105
+  echo "2. Fetching CORL source repository"
+  git clone --branch 0.5 git://github.com/coralnexus/corl.git /tmp/corl >/tmp/corl.git.log 2>&1 || exit 103
+  git submodule update --init --recursive >>/tmp/corl.git.log 2>&1 || exit 104
 
-echo "4. Installing latest CORL"
-gem install /tmp/corl/corl-*.gem >/tmp/corl.install.log 2>&1 || exit 106
+  echo "3. Building CORL gem from branch 0.5"
+  gem build /tmp/corl/corl.gemspec >/tmp/corl.build.log 2>&1 || exit 105
+
+  echo "4. Installing latest CORL"
+  gem install /tmp/corl/corl-*.gem >/tmp/corl.install.log 2>&1 || exit 106
+else
+  echo "1. Removing old versions of CORL gem"
+  gem uninstall corl --force
+  
+  echo "1. Installing latest release of CORL"
+  gem install corl
+fi
