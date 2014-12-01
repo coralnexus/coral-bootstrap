e948a22d5e5933284bf9f5f0a385e2921cfc6be7
Adrian Webb <adrian.webb@coraltech.net>
2014-12-01 02:40:54 -0500

    Updating the nucleon and corl bootstrap scripts to reflect usage of RVM and removing old gems.

--- os/ubuntu/09_nucleon.sh
+++ os/ubuntu/09_nucleon.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
 
-# For now we install Nucleon through the repo for testing purposes
+# Uninstall the nucleon gem
+
+echo "1. Removing old versions of Nucleon gem"
+su - -c "gem uninstall nucleon -x --force" root >/tmp/nucleon.uninstall.log 2>&1 || exit 90
 
 # Install the nucleon gem
 
 if [ "$DEVELOPMENT_BUILD" ]
 then
   rm -Rf /tmp/nucleon
-  mkdir /tmp/nucleon || exit 90
+  mkdir /tmp/nucleon || exit 91
 
-  cd /tmp/nucleon || exit 101
+  cd /tmp/nucleon || exit 92
 
-  echo "1. Fetching Nucleon source repository"
-  git clone --branch 0.2 git://github.com/coralnexus/nucleon.git /tmp/nucleon >/tmp/nucleon.git.log 2>&1 || exit 91
-  git submodule update --init --recursive >>/tmp/nucleon.git.log 2>&1 || exit 92
+  echo "2. Fetching Nucleon source repository"
+  git clone --branch 0.2 git://github.com/coralnexus/nucleon.git /tmp/nucleon >/tmp/nucleon.git.log 2>&1 || exit 93
+  git submodule update --init --recursive >>/tmp/nucleon.git.log 2>&1 || exit 94
 
-  echo "2. Building Nucleon gem from branch 0.2"
-  gem build /tmp/nucleon/nucleon.gemspec >/tmp/nucleon.build.log 2>&1 || exit 93
-
-  echo "3. Installing latest Nucleon"
-  gem install /tmp/nucleon/nucleon-*.gem >/tmp/nucleon.install.log 2>&1 || exit 94
-else
-  echo "1. Removing old versions of Nucleon gem"
-  gem uninstall nucleon --force
+  echo "3. Building Nucleon gem from branch 0.2"
+  su - -c "cd /tmp/nucleon; gem build /tmp/nucleon/nucleon.gemspec" root >/tmp/nucleon.build.log 2>&1 || exit 95
   
+  echo "4. Installing latest dev build of Nucleon"
+  su - -c "cd /tmp/nucleon; gem install /tmp/nucleon/nucleon-*.gem" root >/tmp/nucleon.install.log 2>&1 || exit 96
+else 
   echo "2. Installing latest release of Nucleon"
-  gem install nucleon  
+  su - -c "gem install nucleon" root >/tmp/nucleon.install.log 2>&1 || exit 91  
 fi
--- os/ubuntu/10_corl.sh
+++ os/ubuntu/10_corl.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
 
-# For now we install CORL through the repo for testing purposes
+#if [ `gem list -i nokogiri` != "true" ]
+#then
+    # echo "1. Installing Nokogiri"
+    # su - -c "gem install nokogiri" root >/tmp/nokogiri.install.log 2>&1 || exit 100
+#fi
 
-if [ `gem list -i nokogiri` != "true" ]
-then
-    echo "1. Installing Nokogiri"
-    gem install nokogiri >/tmp/nokogiri.install.log 2>&1 || exit 100
-fi
+# Uninstall the CORL gem
 
-# Install the corl gem
+echo "1. Removing old versions of corl gem"
+su - -c "gem uninstall corl -x --force" root >/tmp/corl.uninstall.log 2>&1 || exit 101
+
+# Install the CORL gem
 
 if [ "$DEVELOPMENT_BUILD" ]
 then
   rm -Rf /tmp/corl
-  mkdir /tmp/corl || exit 101
+  mkdir /tmp/corl || exit 102
 
-  cd /tmp/corl || exit 102
+  cd /tmp/corl || exit 103
 
   echo "2. Fetching CORL source repository"
-  git clone --branch 0.5 git://github.com/coralnexus/corl.git /tmp/corl >/tmp/corl.git.log 2>&1 || exit 103
-  git submodule update --init --recursive >>/tmp/corl.git.log 2>&1 || exit 104
+  git clone --branch 0.5 git://github.com/coralnexus/corl.git /tmp/corl >/tmp/corl.git.log 2>&1 || exit 104
+  git submodule update --init --recursive >>/tmp/corl.git.log 2>&1 || exit 105
 
   echo "3. Building CORL gem from branch 0.5"
-  gem build /tmp/corl/corl.gemspec >/tmp/corl.build.log 2>&1 || exit 105
+  su - -c "cd /tmp/corl; gem build /tmp/corl/corl.gemspec" root >/tmp/corl.build.log 2>&1 || exit 106
 
-  echo "4. Installing latest CORL"
-  gem install /tmp/corl/corl-*.gem >/tmp/corl.install.log 2>&1 || exit 106
+  echo "4. Installing latest dev build of CORL"
+  su - -c "cd /tmp/corl; gem install /tmp/corl/corl-*.gem" root >/tmp/corl.install.log 2>&1 || exit 107
 else
-  echo "1. Removing old versions of CORL gem"
-  gem uninstall corl --force
-  
-  echo "1. Installing latest release of CORL"
-  gem install corl
+  echo "2. Installing latest release of CORL"
+  su - -c "gem install corl" root >/tmp/corl.install.log 2>&1 || exit 102
 fi
