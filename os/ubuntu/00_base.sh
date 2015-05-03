8e2978c21e39ed65eb64476772e619d9cc747d62
Adrian Webb <adrian.webb@coraltech.net>
2015-05-03 02:40:18 -0400

    Adding force of system time update in the base bootstrap script (for Raspberry Pis).

--- os/ubuntu/00_base.sh
+++ os/ubuntu/00_base.sh
@@ -17,14 +17,18 @@ echo "3. Setting command DNS gateways"
 echo "nameserver 208.67.222.222" | tee /etc/resolvconf/resolv.conf.d/base > /dev/null || exit 3
 resolvconf -u || exit 4
 
+# Force time update (for systems without a hardware clock / Raspberry Pi)
+echo "4. Forcing system time update"
+ntpdate -s time.nist.gov || exit 5
+
 # Update system packages
-echo "4. Updating system packages"
-apt-get update >/tmp/update.log 2>&1 || exit 5
+echo "5. Updating system packages"
+apt-get update >/tmp/update.log 2>&1 || exit 6
 
 # Install basic build packages.
-echo "5. Ensuring basic libraries and development utilities"
+echo "6. Ensuring basic libraries and development utilities"
 apt-get -y install build-essential cmake rake bindfs libnl-dev libpopt-dev \
                    libssl-dev libcurl4-openssl-dev libxslt-dev libxml2-dev \
                    libyaml-dev libreadline-dev libncurses5-dev zlib1g-dev texinfo \
-                   llvm llvm-dev python-software-properties unzip curl bison >/tmp/base.install.log 2>&1 || exit 6
+                   llvm llvm-dev python-software-properties unzip curl bison >/tmp/base.install.log 2>&1 || exit 7
      
